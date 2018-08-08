35447f08aefaa05c24ce63b52bfddde50bf38b58
Erik <erikhansson85@gmail.com>
2018-08-08 16:01:27 +0200

    working radiobutton and posting form

--- src/main/java/com/gavlehudik/shopping/ConfirmController.java
+++ src/main/java/com/gavlehudik/shopping/ConfirmController.java
@@ -2,6 +2,10 @@ package com.gavlehudik.shopping;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ConfirmController {
@@ -11,4 +15,19 @@ public class ConfirmController {
     public String getConfirmPage(){
         return "confirm";
     }
+
+    @PostMapping("/confirm")
+    public String postForm(@RequestParam String addressRadio,
+                         @RequestParam String paymentRadio,
+                         HttpServletRequest request){
+
+        String addressOption = addressRadio;
+        String paymentOption = paymentRadio;
+        System.out.println(addressOption + " " + paymentOption);
+
+        return "confirm";
+
+    }
+
+
 }
