ac820c8fc25af07b484bfe0c3bee68f522c16635
Sandra Rehn <sandrarehn@gmail.com>
2018-08-09 15:14:34 +0200

    bugfixes

--- src/main/java/com/gavlehudik/shopping/ILController.java
+++ src/main/java/com/gavlehudik/shopping/ILController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,17 +20,13 @@ public class ILController {
     private ILRepository ilRepository;
 
 
-    @GetMapping("/Index")
+    @GetMapping("/")
     private String getIndex(){
-        return "Index";
+        return "index";
     }
 
-    @GetMapping("/SH")
-    private String getSH() {
-        return "SH";
-    }
 
-    @GetMapping("/LoginTest")
+    @GetMapping("/login")
     public String LogInUser (HttpServletRequest request){
         HttpSession session = request.getSession(true);
         if(session.getAttribute("LoggedIn") !=null) {
@@ -41,7 +37,7 @@ public class ILController {
         return "redirect:/";
     }
 
-    @PostMapping("/Login")
+    @PostMapping("/login")
     public String getUserInput(@RequestParam (name="email", required=true)String email, @RequestParam(name="password")String password,
                                HttpServletRequest request) throws SQLException {
         if(ilRepository.verifyLogin(email,password)) {
@@ -51,11 +47,11 @@ public class ILController {
         return "redirect:/";
     }
 
-    @GetMapping("/Logout")
+    @GetMapping("/logout")
     public String removeSessionCookie(HttpServletResponse respons, HttpServletRequest request){
         HttpSession session = request.getSession(true);
         session.invalidate ();
-        return "Index";
+        return "/";
     }
 }
 
