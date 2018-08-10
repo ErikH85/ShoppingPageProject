609d097333d42c6ea3856696edc1c4ceffb33ea7
Erik <erikhansson85@gmail.com>
2018-08-10 13:03:35 +0200

    init

---
+++ src/main/java/com/gavlehudik/shopping/SHController.java
@@ -0,0 +1,42 @@
+package com.gavlehudik.shopping;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+public class SHController {
+
+    @Autowired
+    private SHRepository shRepository;
+
+
+    @GetMapping("sh-test")
+    public ModelAndView displaySH(HttpServletRequest request){
+
+        HttpSession session = request.getSession(true);
+
+        List<ShoppingCart> shoppingCartInventory;
+
+        if (session.getAttribute("LoggedIn") != null) {
+            System.out.println("inloggad");
+
+
+            int id = (Integer) session.getAttribute("userID");
+
+            shoppingCartInventory = shRepository.getShoppingCart(Integer.toString(id));
+
+            return new ModelAndView("sh-test").addObject("shoppingCartInventory", shoppingCartInventory);
+        }
+        else{
+            return new ModelAndView("sh-test");
+        }
+    }
+
+
+}
