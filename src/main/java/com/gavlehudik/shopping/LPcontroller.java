7a8c83e1de0ab17cc941983a3beb930291c03aff
Christoffer <christoffergunnarsson@hotmail.com>
2018-08-10 15:49:36 +0200

    Added link between shoppingCart and products

--- src/main/java/com/gavlehudik/shopping/LPcontroller.java
+++ src/main/java/com/gavlehudik/shopping/LPcontroller.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LPcontroller {
@@ -25,5 +27,16 @@ public class LPcontroller {
         public String games(Model model) {
             model.addAttribute("Products",LPrepository.getGames());
             return "index";
+        }
+        @GetMapping("/addproduct")
+        public String addProduct(@RequestParam String id, HttpServletRequest request) {
+            HttpSession session = request.getSession(true);
+            if (session.getAttribute("userID") != null) {
+                int userID = (int) session.getAttribute("userID");
+                int productId = Integer.parseInt(id);
+                LPrepository.addProduct(productId,userID);
+                return "redirect:/";
+            }
+            return "redirect:/";
         }
 }
